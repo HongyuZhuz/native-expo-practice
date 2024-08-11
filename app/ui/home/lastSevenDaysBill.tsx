3ebd2e8c0e3ab1dea8077828b61ee128388a7a04
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2024-08-11 15:45:10 +1000

    finishi part of the section header ui

--- app/ui/home/lastSevenDaysBill.tsx
+++ app/ui/home/lastSevenDaysBill.tsx
@@ -4,6 +4,7 @@ import { useState,useEffect } from "react";
 import { getLatestWeekBill } from "@/app/data/database";
 import { Bill, BillIncludeAccountName,Section } from "@/assets/definition";
 import { groupBillsByDate } from "@/app/data/calculate";
+import { FormattedDate, FormattedDateParts, IntlProvider} from "react-intl";
 
 
 export default function LastSevenDayBillsSectionList() {
@@ -81,9 +82,37 @@ function ListItem ({bill}:{bill:Bill}) {
 
 function SectionHeader({section}:{section:Section}) {
   return(
-    <View style={styles.sectionHeader}>
-      <Text style={{color:'white'}}>{section.title}</Text>
+    <IntlProvider locale='en'>
+      <View style={styles.sectionHeader}>
+        <View style={styles.verticalLine}/>
+        <Text style={{color:'white'}}><FormatDate dateString={section.title}/></Text>
+        
+      
     </View>
+    </IntlProvider>
+    
+  )
+}
+
+function FormatDate ({dateString}:{dateString:string}) {
+  const date = new Date(dateString);
+  const now  = new Date();
+  const yesterday = new Date(now);
+  yesterday.setDate(now.getDate()-1);
+
+  let label = '';
+  if(date.toDateString()===now.toDateString()){
+    label = 'Today';
+  }else if (date.toDateString()===yesterday.toDateString()){
+    label='Yesterday'
+  }
+
+
+
+  return(
+    <Text style={styles.sectionHeaderText}>{label+" "}
+    <FormattedDateParts value={date} month="short" day="numeric" weekday="short">{parts =>(<><Text>{parts[3].value+" "+ parts[5].value+" "+parts[0].value}</Text></>)}</FormattedDateParts>
+    </Text>
   )
 }
 
@@ -121,6 +150,18 @@ const styles = StyleSheet.create({
       flexDirection:'row',
       paddingHorizontal:20,
       paddingVertical:5,
-    }
+    },
+    sectionHeaderText:{
+      color:'white',
+      fontSize:10,
+      fontWeight:'600'
+    },
+    verticalLine: {
+      width: 3, 
+      height: '100%',
+      backgroundColor: 'orange', 
+      marginRight: 5, 
+      borderRadius:10,
+    },
 })
 
