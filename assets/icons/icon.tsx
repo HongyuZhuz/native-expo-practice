54b5388715fb3978db88cfef3f357cae0ab7562a
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2024-08-15 12:50:43 +1000

    set the icno tst

--- assets/icons/icon.tsx
+++ assets/icons/icon.tsx
@@ -1,18 +1,48 @@
 
-
-import Shopping from './shopping.svg'
 import { SvgProps } from 'react-native-svg';
+import Shopping from './shopping.svg'
 import Sale from './sale.svg'
 import Transfer from './transfer.svg'
 import Undefined from './undefined.svg'
+import Breakfast from './breakfast.svg'
+import Lunch from './lunch.svg'
+import Dinner from './dinner.svg'
+import Drink from './drink.svg'
+import Camera from './camera.svg'
+import Car from './car.svg'
+import Dining from './dining.svg'
+import Groceries from './groceries.svg'
+import Petrol from './petrol.svg'
+
+
+
 
 const iconMap: { [key: string]:  React.FC<SvgProps> } = {
     shopping: Shopping,
     sale:Sale,
     transfer:Transfer,
     undefined:Undefined,
+    breakfast:Breakfast,
+    camera:Camera,
+    car:Car,
+    dining:Dining,
+    dinner:Dinner,
+    drink:Drink,
+    groceries:Groceries,
+    lunch:Lunch,
+    petrol:Petrol
+
   };
 
+  const iconLib:{[key:string]:{[key:string]:string[]}} = {
+    expense:{
+      groceries:[],
+      dinning:["breakfast","lunch","dinner","drink"],
+      snacks:[],
+      transport:["petrol","car"],
+      shopping:["camera"]
+    }
+  }
 
   export function Icon({ name }: { name: string }) {
     // 根据 `name` 动态选择组件
